189800b358655850740ad1b4597bf14d7a3b8055
arifrahmadi23 <muhammad231arif@gmail.com>
2025-01-18 20:14:11 +0700

    Membuat data class Villa

---
+++ app/src/main/java/com/example/myapplication/model/Villa.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Villa (
+    val id_villa: String,
+    val nama_villa: String,
+    val alamat: String,
+    val kamar_tersedia: String
+)
+
