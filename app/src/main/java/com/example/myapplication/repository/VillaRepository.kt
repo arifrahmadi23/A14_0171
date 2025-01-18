ddce9a6e6d7674ad67f1fbb930c8c20b34013b11
arifrahmadi23 <muhammad231arif@gmail.com>
2025-01-18 21:18:43 +0700

    Menambahkan fungsi override VillaRepository

--- app/src/main/java/com/example/myapplication/repository/VillaRepository.kt
+++ app/src/main/java/com/example/myapplication/repository/VillaRepository.kt
@@ -2,6 +2,8 @@ package com.example.myapplication.repository
 
 import com.example.myapplication.model.AllVillaRespose
 import com.example.myapplication.model.Villa
+import com.example.myapplication.service.VillaService
+import java.io.IOException
 
 interface VillaRepository {
     suspend fun getVilla(): AllVillaRespose
@@ -13,5 +15,39 @@ interface VillaRepository {
     suspend fun deleteVilla(id_villa: String)
 
     suspend fun getVillaById(id_villa: String): Villa
+}
+
+class NetworkVillaRepository(
+    private val villaApiService: VillaService
+) : VillaRepository{
+    override suspend fun getVilla(): AllVillaRespose =
+        villaApiService.getAllVilla()
+
+    override suspend fun getVillaById(id_villa: String): Villa {
+        return villaApiService.getVillabyId(id_villa).data
+    }
+
+    override suspend fun insertVilla(villa: Villa) {
+        villaApiService.insertVilla(villa)
+    }
+
+    override suspend fun updateVilla(id_villa: String, villa: Villa) {
+        villaApiService.updateVilla(id_villa, villa)
+    }
+
+    override suspend fun deleteVilla(id_villa: String) {
+        try{
+            val response = villaApiService.deleteVilla(id_villa)
+            if (!response.isSuccessful){
+                throw IOException("Failed to delete Villa. HTTP Status Code: " +
+                        "${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        }catch (e:Exception){
+            throw e
+        }
+    }
 
 }
