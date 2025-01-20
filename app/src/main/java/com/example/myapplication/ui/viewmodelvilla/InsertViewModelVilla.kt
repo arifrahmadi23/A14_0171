b1c474508b30d325ab4877101d5dd6e16069ca77
arifrahmadi23 <muhammad231arif@gmail.com>
2025-01-20 16:30:32 +0700

    Membuat InsertViewModelVilla

---
+++ app/src/main/java/com/example/myapplication/ui/viewmodelvilla/InsertViewModelVilla.kt
@@ -0,0 +1,51 @@
+package com.example.myapplication.ui.viewmodelvilla
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.myapplication.model.Villa
+import com.example.myapplication.repository.VillaRepository
+
+
+class InsertViewModelVilla (private val villa: VillaRepository): ViewModel(){
+    var uiStateVilla by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent){
+        uiStateVilla = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+    suspend fun insertMhs() {
+        try {
+            villa.insertVilla(uiStateVilla.insertUiEvent.toVilla())
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
+
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent()
+)
+
+data class InsertUiEvent(
+    val id_villa: Int,
+    val nama_villa: String = "",
+    val alamat: String = "",
+    val kamar_tersedia: String = ""
+)
+
+fun InsertUiEvent.toVilla():Villa = Villa (
+    id_villa = id_villa,
+    nama_villa = nama_villa,
+    alamat = alamat,
+    kamar_tersedia = kamar_tersedia
+)
+
+
+fun Villa.toInsertUiEvent(): InsertUiEvent = InsertUiEvent (
+    id_villa = id_villa,
+    nama_villa = nama_villa,
+    alamat = alamat,
+    kamar_tersedia = kamar_tersedia
+)
