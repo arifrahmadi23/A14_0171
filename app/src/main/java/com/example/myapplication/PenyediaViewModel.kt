b59d4f0fc6cbb7b4a1c3cd9a8e02d35ca3e127e8
arifrahmadi23 <muhammad231arif@gmail.com>
2025-01-20 16:38:53 +0700

    Menambahkan InsertViewModelVilla pada Penyedia ViewModel

--- app/src/main/java/com/example/myapplication/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myapplication/PenyediaViewModel.kt
@@ -5,11 +5,16 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.myapplication.ui.viewmodelvilla.HomeViewModelVilla
+import com.example.myapplication.ui.viewmodelvilla.InsertViewModelVilla
 
 object PenyediaViewModel{
     val Factory = viewModelFactory {
         initializer {
             HomeViewModelVilla(aplikasiVilla().container.villaRepository)
+        }
+
+        initializer {
+            InsertViewModelVilla(aplikasiVilla().container.villaRepository)
         }
     }
 }
