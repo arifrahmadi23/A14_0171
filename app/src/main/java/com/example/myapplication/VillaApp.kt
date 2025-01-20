2b2f8dd6537d18344b246b518e215a15b5a2064d
arifrahmadi23 <muhammad231arif@gmail.com>
2025-01-20 16:22:15 +0700

    Membuat VillaApp

--- app/src/main/java/com/example/myapplication/VillaApp.kt
+++ app/src/main/java/com/example/myapplication/VillaApp.kt
@@ -1,13 +1,29 @@
 package com.example.myapplication
 
-import android.app.Application
-import com.example.myapplication.dependeciesinjection.AppContainer
-import com.example.myapplication.dependeciesinjection.Container
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import com.example.myapplication.navigation.PengelolaHalaman
 
-class VillaApp: Application(){
-    lateinit var container: AppContainer
-    override fun onCreate() {
-        super.onCreate()
-        container = Container()
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun VillaApp(modifier: Modifier = Modifier){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold (
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+    ){
+        Surface (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ){
+            PengelolaHalaman()
+        }
     }
 }
