f67a6804b56962f45a3010809f5c5581ddbb845c
arifrahmadi23 <muhammad231arif@gmail.com>
2025-01-20 16:21:10 +0700

    Menambahkan HomeStatus dan HomeScreen pada HomeViewVilla

--- app/src/main/java/com/example/myapplication/ui/viewvilla/HomeViewVilla.kt
+++ app/src/main/java/com/example/myapplication/ui/viewvilla/HomeViewVilla.kt
@@ -3,6 +3,7 @@ package com.example.myapplication.ui.viewvilla
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -13,24 +14,105 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.myapplication.PenyediaViewModel
 import com.example.myapplication.R
+import com.example.myapplication.customwidget.CostumeTopAppBar
 import com.example.myapplication.model.Villa
+import com.example.myapplication.navigation.DestinasiNavigasi
+import com.example.myapplication.ui.viewmodelvilla.HomeViewModelVilla
+import com.example.myapplication.ui.viewmodelvilla.HomeVillaUiState
 
+object DestinasiHomeVilla: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home Villa"
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry:()->Unit,
+    modifier: Modifier=Modifier,
+    onDetailClick: (String) -> Unit ={},
+    viewModel: HomeViewModelVilla = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiHomeVilla.titleRes,
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior,
+                onRefresh = {
+                    viewModel.getVilla()
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Tambah Villa")
+            }
+        },
+    ) { innerPadding->
+        HomeStatus(
+            homeVillaUiState = viewModel.villaUiState,
+            retryAction = {viewModel.getVilla()}, modifier = Modifier.padding(innerPadding),
+            onDetailClick = {}
+        )
+    }
+}
 
+@Composable
+fun HomeStatus(
+    homeVillaUiState: HomeVillaUiState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit
+){
+    when (homeVillaUiState){
+        is HomeVillaUiState.Loading-> OnLoading(modifier = modifier.fillMaxSize())
+
+        is HomeVillaUiState.Success ->
+            if(homeVillaUiState.villa.isEmpty()){
+                return Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+                    Text(text = "Tidak ada data villa")
+                }
+            }else{
+                VillaLayout(
+                    villa = homeVillaUiState.villa,modifier = modifier.fillMaxWidth(),
+                    onDetailClick = {
+                        onDetailClick(it.id_villa.toString())
+                    },
+                )
+            }
+        is HomeVillaUiState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
+    }
+}
 
 
 @Composable
@@ -38,7 +120,7 @@ fun OnLoading(modifier: Modifier = Modifier){
     Image(
         modifier = modifier.size(200.dp),
         painter = painterResource(R.drawable.loading_img),
-        contentDescription = stringResource("Loading")
+        contentDescription = stringResource(R.string.loading)
     )
 }
 
@@ -52,9 +134,9 @@ fun OnError(retryAction:() -> Unit, modifier: Modifier = Modifier){
         Image(
             painter = painterResource(id = R.drawable.ic_connection_error), contentDescription = ""
         )
-        Text(text = stringResource("Failed to load"), modifier = Modifier.padding(16.dp))
+        Text(text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp))
         Button(onClick = retryAction) {
-            Text(stringResource("Retry"))
+            Text(stringResource(R.string.retry))
         }
     }
 }
@@ -111,7 +193,7 @@ fun VillaCard(
                 style = MaterialTheme.typography.titleMedium
             )
             Text(
-                text = villa.kamar_tersedia + "Kamar Tesedia",
+                text = villa.kamar_tersedia + " Kamar Tesedia",
                 style = MaterialTheme.typography.titleMedium
             )
         }
