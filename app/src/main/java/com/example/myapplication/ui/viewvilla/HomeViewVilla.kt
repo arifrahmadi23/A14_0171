162aa777a1172b224a0a20e6bfe9e9a2d1f34a4c
arifrahmadi23 <muhammad231arif@gmail.com>
2025-01-20 14:02:03 +0700

    Membuat Tampilan loading dan error pada HomeViewVilla dan menambahkan gambar

--- app/src/main/java/com/example/myapplication/ui/viewvilla/HomeViewVilla.kt
+++ app/src/main/java/com/example/myapplication/ui/viewvilla/HomeViewVilla.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication.ui.viewvilla
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,18 +10,54 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.myapplication.R
 import com.example.myapplication.model.Villa
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+
+
+
+@Composable
+fun OnLoading(modifier: Modifier = Modifier){
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.loading_img),
+        contentDescription = stringResource("Loading")
+    )
+}
+
+@Composable
+fun OnError(retryAction:() -> Unit, modifier: Modifier = Modifier){
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.ic_connection_error), contentDescription = ""
+        )
+        Text(text = stringResource("Failed to load"), modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction) {
+            Text(stringResource("Retry"))
+        }
+    }
+}
 
 @Composable
 fun VillaLayout(
