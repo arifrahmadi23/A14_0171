dae6a0c91c060ab5bc65b498511a2061997f4ab9
arifrahmadi23 <muhammad231arif@gmail.com>
2025-01-20 14:10:27 +0700

    Membuat VillaApp

---
+++ app/src/main/java/com/example/myapplication/VillaApp.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+import android.app.Application
+import com.example.myapplication.dependeciesinjection.AppContainer
+import com.example.myapplication.dependeciesinjection.Container
+
+class VillaApp: Application(){
+    lateinit var container: AppContainer
+    override fun onCreate() {
+        super.onCreate()
+        container = Container()
+    }
+}
