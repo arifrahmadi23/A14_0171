dbd6c119f9e59859f2e0c9c50156104b629b21f4
arifrahmadi23 <muhammad231arif@gmail.com>
2025-01-20 11:31:12 +0700

    Membuat HomeViewModelVilla

---
+++ app/src/main/java/com/example/myapplication/ui/viewmodelvilla/HomeViewModelVilla.kt
@@ -0,0 +1,40 @@
+package com.example.myapplication.ui.viewmodelvilla
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
+import com.example.myapplication.model.Villa
+import com.example.myapplication.repository.VillaRepository
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed class HomeVillaUiState{
+    data class Success(val villa: List<Villa>): HomeVillaUiState()
+    object Error : HomeVillaUiState()
+    object Loading : HomeVillaUiState()
+}
+
+class HomeViewModelVilla(private val villa:VillaRepository): ViewModel(){
+    var villaUiState: HomeVillaUiState by mutableStateOf(HomeVillaUiState.Loading)
+        private set
+
+    init {
+        getVilla()
+    }
+
+    fun getVilla(){
+        viewModelScope.launch {
+            villaUiState = HomeVillaUiState.Loading
+            villaUiState = try {
+                HomeVillaUiState.Success(villa.getVilla().data)
+            }catch (e : IOException){
+                HomeVillaUiState.Error
+            }catch (e : HttpException){
+                HomeVillaUiState.Error
+            }
+        }
+    }
+}
