fdbab5432ae441bb3253c06c844e7b6aa7a278ec
arifrahmadi23 <muhammad231arif@gmail.com>
2025-01-20 13:56:38 +0700

    Membuat VillaLayout pada HomeViewVilla

--- app/src/main/java/com/example/myapplication/ui/viewvilla/HomeViewVilla.kt
+++ app/src/main/java/com/example/myapplication/ui/viewvilla/HomeViewVilla.kt
@@ -1,12 +1,16 @@
 package com.example.myapplication.ui.viewvilla
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -16,6 +20,31 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.model.Villa
+
+
+@Composable
+fun VillaLayout(
+    villa: List<Villa>,
+    modifier: Modifier = Modifier,
+    onDetailClick:(Villa) -> Unit,
+){
+    LazyColumn (
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ){
+        items(villa){
+                villa ->
+            VillaCard(
+                villa = villa,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onDetailClick(villa) },
+
+            )
+        }
+    }
+}
 
 @Composable
 fun VillaCard(
