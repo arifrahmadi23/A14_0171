fc7668d341b9f2c516a8716842098ceb0c73393c
arifrahmadi23 <muhammad231arif@gmail.com>
2025-01-22 06:11:14 +0700

    Menambahkan Container untuk menghubungkan ke backed Pelanggan

--- app/src/main/java/com/example/myapplication/dependeciesinjection/Container.kt
+++ app/src/main/java/com/example/myapplication/dependeciesinjection/Container.kt
@@ -1,30 +1,49 @@
 package com.example.myapplication.dependeciesinjection
 
+import com.example.myapplication.repository.NetworkPelangganRepository
 import com.example.myapplication.repository.NetworkVillaRepository
+import com.example.myapplication.repository.PelangganRepository
 import com.example.myapplication.repository.VillaRepository
+import com.example.myapplication.service.PelangganService
 import com.example.myapplication.service.VillaService
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
 
-interface AppContainer{
+interface AppContainer {
     val villaRepository: VillaRepository
+    val pelangganRepository: PelangganRepository
 }
 
-class Container : AppContainer{
+class Container : AppContainer {
 
-    private val baseUrl = "http://10.0.2.2:3000/api/villa/"
+    private val baseUrlVilla = "http://10.0.2.2:3000/api/villa/"
+    private val baseUrlPelanggan = "http://10.0.2.2:3000/api/pelanggan/"
     private val json = Json { ignoreUnknownKeys = true }
-    private val retroit: Retrofit = Retrofit.Builder()
-        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
-        .baseUrl(baseUrl).build()
+
+    // Create the Retrofit instance
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+            .baseUrl(baseUrlVilla)  // You can change the base URL as needed when needed per service
+            .build()
+    }
 
     private val villaService: VillaService by lazy {
-        retroit.create(VillaService::class.java)
+        retrofit.create(VillaService::class.java)
+    }
+
+    private val pelangganService: PelangganService by lazy {
+        retrofit.newBuilder().baseUrl(baseUrlPelanggan).build().create(PelangganService::class.java)
     }
 
     override val villaRepository: VillaRepository by lazy {
         NetworkVillaRepository(villaService)
     }
+
+    override val pelangganRepository: PelangganRepository by lazy {
+        NetworkPelangganRepository(pelangganService)
+    }
+
 }
