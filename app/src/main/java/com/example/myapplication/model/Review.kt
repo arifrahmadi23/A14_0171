592c6847492fee57b7bb612fbc03072e98f5b824
arifrahmadi23 <muhammad231arif@gmail.com>
2025-01-26 01:11:32 +0700

    Membuat model Review

---
+++ app/src/main/java/com/example/myapplication/model/Review.kt
@@ -0,0 +1,26 @@
+package com.example.myapplication.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Review(
+    val id_review: Int,
+    val id_reservasi: Int,
+    val nilai: String,
+    val komentar: String
+
+)
+
+@Serializable
+data class AllReviewResponse(
+    val status: Boolean,
+    val message: String,
+    val data: List<Review>
+)
+
+@Serializable
+data class ReviewDetailResponse(
+    val status: Boolean,
+    val message: String,
+    val data: Review
+)
