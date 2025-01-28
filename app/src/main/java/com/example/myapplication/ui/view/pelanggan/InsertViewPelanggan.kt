636bcfdf817216f8b28d790b29eeb37cf0dcef1f
arifrahmadi23 <muhammad231arif@gmail.com>
2025-01-28 18:36:59 +0700

    Menambahkan validasi untuk insert pelanggan

--- app/src/main/java/com/example/myapplication/ui/view/pelanggan/InsertViewPelanggan.kt
+++ app/src/main/java/com/example/myapplication/ui/view/pelanggan/InsertViewPelanggan.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -26,13 +27,17 @@ import com.example.myapplication.navigation.DestinasiNavigasi
 import com.example.myapplication.ui.view.villa.DestinasiEntryVilla
 import com.example.myapplication.ui.view.villa.EntryBody
 import com.example.myapplication.ui.view.villa.FormInputVilla
+import com.example.myapplication.ui.viewmodel.pelanggan.FormErrorStatePelanggan
 import com.example.myapplication.ui.viewmodel.pelanggan.InsertUiEventPelanggan
 import com.example.myapplication.ui.viewmodel.pelanggan.InsertUiStatePelanggan
 import com.example.myapplication.ui.viewmodel.pelanggan.InsertViewModelPelanggan
 import com.example.myapplication.ui.viewmodel.villa.InsertUiEvent
 import com.example.myapplication.ui.viewmodel.villa.InsertUiState
 import com.example.myapplication.ui.viewmodel.villa.InsertViewModelVilla
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 object DestinasiEntryPelanggan : DestinasiNavigasi {
     override val route = "item_entryPelanggan"
@@ -64,8 +69,13 @@ fun EntryPelangganScreen(
             onPelangganValueChange = viewModel::updateInsertPelangganState,
             onSaveClick = {
                 coroutineScope.launch {
-                    viewModel.insertPelanggan()
-                    navigateBack()
+                    if (viewModel.validateFieldsPelanggan()) {
+                        viewModel.insertPelanggan()
+                        delay(500)
+                        withContext(Dispatchers.Main) {
+                            navigateBack()
+                        }
+                    }
                 }
             },
             modifier = Modifier
@@ -90,6 +100,7 @@ fun EntryBodyPelanggan(
         FormInputPelanggan(
             insertUiEventPelanggan = insertUiStatePelanggan.insertUiEventPelanggan,
             onValueChange = onPelangganValueChange,
+            errorState = insertUiStatePelanggan.isEntryValid,
             modifier = Modifier.fillMaxWidth()
         )
         Button(
@@ -106,6 +117,7 @@ fun EntryBodyPelanggan(
 fun FormInputPelanggan(
     insertUiEventPelanggan: InsertUiEventPelanggan,
     modifier: Modifier = Modifier,
+    errorState: FormErrorStatePelanggan = FormErrorStatePelanggan(),
     onValueChange: (InsertUiEventPelanggan) -> Unit = {},
     enabled: Boolean = true
 ){
@@ -117,18 +129,28 @@ fun FormInputPelanggan(
             value = insertUiEventPelanggan.nama_pelanggan,
             onValueChange = {onValueChange(insertUiEventPelanggan.copy(nama_pelanggan = it))},
             label = { Text("Nama Pelanggan") },
+            isError = errorState.nama_pelanggan != null,
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
             singleLine = true
+        )
+        Text(
+            text = errorState.nama_pelanggan ?: "",
+            color = Color.Red
         )
 
         OutlinedTextField(
             value = insertUiEventPelanggan.no_hp,
             onValueChange = {onValueChange(insertUiEventPelanggan.copy(no_hp = it))},
             label = { Text("Nomor Handphone") },
+            isError = errorState.no_hp != null ,
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
             singleLine = true
+        )
+        Text(
+            text = errorState.no_hp ?: "",
+            color = Color.Red
         )
 
         if (enabled){
--- app/src/main/java/com/example/myapplication/ui/viewmodel/pelanggan/InsertViewModelPelanggan.kt
+++ app/src/main/java/com/example/myapplication/ui/viewmodel/pelanggan/InsertViewModelPelanggan.kt
@@ -8,6 +8,7 @@ import com.example.myapplication.model.Pelanggan
 import com.example.myapplication.model.Villa
 import com.example.myapplication.repository.PelangganRepository
 import com.example.myapplication.repository.VillaRepository
+import com.example.myapplication.ui.viewmodel.villa.FormErrorStateVilla
 import com.example.myapplication.ui.viewmodel.villa.InsertUiEvent
 import com.example.myapplication.ui.viewmodel.villa.InsertUiState
 import com.example.myapplication.ui.viewmodel.villa.toVilla
@@ -20,17 +21,44 @@ class InsertViewModelPelanggan (private val pelanggan: PelangganRepository): Vie
     fun updateInsertPelangganState(insertUiEventPelanggan: InsertUiEventPelanggan){
         uiStatePelanggan = InsertUiStatePelanggan(insertUiEventPelanggan = insertUiEventPelanggan)
     }
+
+    fun validateFieldsPelanggan(): Boolean {
+        val event = uiStatePelanggan.insertUiEventPelanggan
+        val errorState = FormErrorStatePelanggan(
+            nama_pelanggan = if  (event.nama_pelanggan.isNotEmpty()) null else "Nama tidak boleh kosong",
+            no_hp = if (event.no_hp.isNotEmpty()) null else "Nomor Handphone tidak bolej kosong"
+        )
+        uiStatePelanggan = uiStatePelanggan.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
     suspend fun insertPelanggan() {
-        try {
-            pelanggan.insertPelanggan(uiStatePelanggan.insertUiEventPelanggan.toPelanggan())
-        } catch (e: Exception) {
-            e.printStackTrace()
+        val currentEvent = uiStatePelanggan.insertUiEventPelanggan
+        if (validateFieldsPelanggan()) {
+            try {
+                pelanggan.insertPelanggan(uiStatePelanggan.insertUiEventPelanggan.toPelanggan())
+                uiStatePelanggan = uiStatePelanggan.copy(
+                    snackBarMessage = "Data berhasil disimpan",
+                    insertUiEventPelanggan = InsertUiEventPelanggan(),
+                    isEntryValid = FormErrorStatePelanggan()
+                )
+            } catch (e: Exception) {
+                uiStatePelanggan = uiStatePelanggan.copy(
+                    snackBarMessage = "Data gagal disimpan"
+                )
+            }
+        } else {
+            uiStatePelanggan = uiStatePelanggan.copy(
+                snackBarMessage = "Input tidak valid. Periksa kembali data anda"
+            )
         }
     }
 }
 
 data class InsertUiStatePelanggan(
-    val insertUiEventPelanggan: InsertUiEventPelanggan = InsertUiEventPelanggan()
+    val insertUiEventPelanggan: InsertUiEventPelanggan = InsertUiEventPelanggan(),
+    val isEntryValid: FormErrorStatePelanggan = FormErrorStatePelanggan(),
+    val snackBarMessage: String? = null
 )
 
 data class InsertUiEventPelanggan(
@@ -38,12 +66,24 @@ data class InsertUiEventPelanggan(
     val nama_pelanggan: String = "",
     val no_hp: String = ""
 )
+data class FormErrorStatePelanggan(
+    val nama_pelanggan: String? = null,
+    val no_hp: String? = null
+){
+    fun isValid(): Boolean{
+        return nama_pelanggan == null && no_hp == null
+    }
+}
+
+
 
 fun InsertUiEventPelanggan.toPelanggan():Pelanggan = Pelanggan (
     id_pelanggan = id_pelanggan,
     nama_pelanggan = nama_pelanggan,
     no_hp = no_hp
 )
+
+
 
 
 fun Pelanggan.toInsertUiEvent(): InsertUiEventPelanggan = InsertUiEventPelanggan (
