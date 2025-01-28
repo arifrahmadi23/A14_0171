050bf52435286a75141b4dd5ccae30315fc7472e
arifrahmadi23 <muhammad231arif@gmail.com>
2025-01-28 18:36:48 +0700

    Menambahkan validasi untuk insert villa

--- app/src/main/java/com/example/myapplication/ui/view/villa/InsertViewVilla.kt
+++ app/src/main/java/com/example/myapplication/ui/view/villa/InsertViewVilla.kt
@@ -17,16 +17,21 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.myapplication.PenyediaViewModel
 import com.example.myapplication.customwidget.CostumeTopAppBar
 import com.example.myapplication.navigation.DestinasiNavigasi
+import com.example.myapplication.ui.viewmodel.villa.FormErrorStateVilla
 import com.example.myapplication.ui.viewmodel.villa.InsertUiEvent
 import com.example.myapplication.ui.viewmodel.villa.InsertUiState
 import com.example.myapplication.ui.viewmodel.villa.InsertViewModelVilla
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 object DestinasiEntryVilla : DestinasiNavigasi {
@@ -59,8 +64,13 @@ fun EntryVillaScreen(
             onVillaValueChange = viewModel::updateInsertMhsState,
             onSaveClick = {
                 coroutineScope.launch {
-                    viewModel.insertMhs()
-                    navigateBack()
+                    if (viewModel.valideteFieldsVilla()){
+                        viewModel.insertVilla()
+                        delay(500)
+                        withContext(Dispatchers.Main){
+                            navigateBack()
+                        }
+                    }
                 }
             },
             modifier = Modifier
@@ -85,6 +95,7 @@ fun EntryBody(
         FormInputVilla(
             insertUiEvent = insertUiState.insertUiEvent,
             onValueChange = onVillaValueChange,
+            errorState = insertUiState.isEntryValid,
             modifier = Modifier.fillMaxWidth()
         )
         Button(
@@ -103,6 +114,7 @@ fun FormInputVilla(
     insertUiEvent: InsertUiEvent,
     modifier: Modifier = Modifier,
     onValueChange: (InsertUiEvent) -> Unit = {},
+    errorState: FormErrorStateVilla = FormErrorStateVilla(),
     enabled: Boolean = true
 ){
     Column (
@@ -113,18 +125,28 @@ fun FormInputVilla(
             value = insertUiEvent.nama_villa,
             onValueChange = {onValueChange(insertUiEvent.copy(nama_villa = it))},
             label = { Text("Nama Villa") },
+            isError = errorState.nama_villa != null,
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
             singleLine = true
+        )
+        Text(
+            text = errorState.nama_villa ?: "",
+            color = Color.Red
         )
 
         OutlinedTextField(
             value = insertUiEvent.alamat,
             onValueChange = {onValueChange(insertUiEvent.copy(alamat = it))},
             label = { Text("Alamat") },
+            isError = errorState.alamat != null,
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
             singleLine = true
+        )
+        Text(
+            text = errorState.alamat ?: "",
+            color = Color.Red
         )
 
         OutlinedTextField(
@@ -134,10 +156,16 @@ fun FormInputVilla(
                 onValueChange(insertUiEvent.copy(kamar_tersedia = kamarTersediaInt))
             },
             label = { Text("Jumlah Kamar") },
+            isError = errorState.kamar_tersedia != null,
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
             singleLine = true
         )
+        Text(
+            text = errorState.kamar_tersedia ?: "",
+            color = Color.Red
+        )
+
         if (enabled){
             Text(
                 text = "Isi Semua Data!",
--- app/src/main/java/com/example/myapplication/ui/viewmodel/villa/InsertViewModelVilla.kt
+++ app/src/main/java/com/example/myapplication/ui/viewmodel/villa/InsertViewModelVilla.kt
@@ -4,28 +4,64 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.myapplication.model.Villa
 import com.example.myapplication.repository.VillaRepository
+import kotlinx.coroutines.launch
 
 
-class InsertViewModelVilla (private val villa: VillaRepository): ViewModel(){
+class InsertViewModelVilla (private val villa: VillaRepository): ViewModel() {
     var uiStateVilla by mutableStateOf(InsertUiState())
         private set
 
-    fun updateInsertMhsState(insertUiEvent: InsertUiEvent){
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent) {
         uiStateVilla = InsertUiState(insertUiEvent = insertUiEvent)
     }
-    suspend fun insertMhs() {
-        try {
-            villa.insertVilla(uiStateVilla.insertUiEvent.toVilla())
-        } catch (e: Exception) {
-            e.printStackTrace()
+
+    fun valideteFieldsVilla(): Boolean {
+        val event = uiStateVilla.insertUiEvent
+        val errorState = FormErrorStateVilla(
+            nama_villa = if (event.nama_villa.isNotEmpty()) null else "Nama Villa tidak boleh kosong",
+            alamat = if (event.alamat.isNotEmpty()) null else "Alamat Villa tidak boleh kosong",
+            kamar_tersedia = if (event.kamar_tersedia.toString().isNotEmpty()) null else "Alamat Villa tidak boleh kosong",
+
+        )
+        uiStateVilla = uiStateVilla.copy(isEntryValid = errorState)
+        return errorState.isValid()
+
+    }
+
+    fun insertVilla() {
+        val currentEvent = uiStateVilla.insertUiEvent
+        if (valideteFieldsVilla()) {
+            viewModelScope.launch {
+                try {
+                    villa.insertVilla(uiStateVilla.insertUiEvent.toVilla())
+                    uiStateVilla = uiStateVilla.copy(
+                        snackBarMessage = "Data berhasil disimpan",
+                        insertUiEvent = InsertUiEvent(),
+                        isEntryValid = FormErrorStateVilla()
+                    )
+                } catch (e: Exception) {
+                    uiStateVilla = uiStateVilla.copy(
+                        snackBarMessage = "Data gagal disimpan"
+                    )
+                }
+            }
+        } else {
+            uiStateVilla = uiStateVilla.copy(
+                snackBarMessage = "Input tidak valid. Periksa kembali data anda"
+            )
         }
     }
 }
 
+
+
 data class InsertUiState(
-    val insertUiEvent: InsertUiEvent = InsertUiEvent()
+    val insertUiEvent: InsertUiEvent = InsertUiEvent(),
+    val isEntryValid: FormErrorStateVilla = FormErrorStateVilla(),
+    val snackBarMessage: String? = null
 )
 
 data class InsertUiEvent(
@@ -34,6 +70,17 @@ data class InsertUiEvent(
     val alamat: String = "",
     val kamar_tersedia: Int = 0
 )
+
+data class FormErrorStateVilla(
+    val nama_villa: String? = null,
+    val alamat: String? = null,
+    val kamar_tersedia: String? = null
+){
+    fun isValid(): Boolean{
+        return nama_villa == null && alamat == null  && kamar_tersedia == null
+    }
+}
+
 
 fun InsertUiEvent.toVilla():Villa = Villa (
     id_villa = id_villa,
